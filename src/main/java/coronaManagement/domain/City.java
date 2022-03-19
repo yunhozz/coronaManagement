ab3f722d9199d7c3e0ffeb82702d1d4bba89d324
yunhozz <qkrdbsgh1121@naver.com>
2022-03-19 19:56:22 +0900

    first commit

---
+++ src/main/java/coronaManagement/domain/City.java
@@ -0,0 +1,23 @@
+package coronaManagement.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class City extends Nation {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "city_id")
+    private Long id;
+
+
+}
---
+++ src/main/java/coronaManagement/domain/Vaccine.java
@@ -0,0 +1,25 @@
+package coronaManagement.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Vaccine {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "vaccine_id")
+    private Long id;
+
+    private String name;
+
+    private int stockQuantity;
+}
