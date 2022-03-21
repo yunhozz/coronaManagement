c9bd0fd06000610fb7690235167f5fddcb9973a9
yunhozz <qkrdbsgh1121@naver.com>
2022-03-21 10:27:30 +0900

    first commit

--- src/main/java/coronaManagement/domain/Vaccine.java
+++ src/main/java/coronaManagement/domain/Vaccine.java
@@ -4,9 +4,10 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 @Getter
@@ -17,30 +18,4 @@ public class Vaccine {
     @GeneratedValue
     @Column(name = "vaccine_id")
     private Long id;
-
-    @OneToMany(mappedBy = "vaccine")
-    private List<NationVaccine> nationVaccines = new ArrayList<>();
-
-    private String name;
-
-    private int stockQuantity;
-
-    public void addQuantity(int stockQuantity) {
-        this.stockQuantity += stockQuantity;
-    }
-
-    public void removeQuantity(int stockQuantity) {
-        this.stockQuantity -= stockQuantity;
-    }
-
-    //치명률 : (감염자 수) / (백신 접종자 수) * 100
-    public double calculateFatalityRate() {
-        double rate = 0;
-
-        for (NationVaccine nationVaccine : nationVaccines) {
-            rate += (double) nationVaccine.getNation().getNumOfInfection() / nationVaccine.getNation().getNumOfVaccination() * 100;
-        }
-
-        return rate / nationVaccines.size();
-    }
 }
