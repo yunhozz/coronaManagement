6a5f6f0c320cc7e0ab786ae737d65934cdda5388
yunhozz <qkrdbsgh1121@naver.com>
2022-03-23 15:15:50 +0900

    first commit

--- src/main/java/coronaManagement/domain/person/PersonBeforeVaccination.java
+++ src/main/java/coronaManagement/domain/person/PersonBeforeVaccination.java
@@ -1,6 +1,9 @@
 package coronaManagement.domain.person;
 
+import coronaManagement.domain.City;
+import coronaManagement.domain.Gender;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,4 +15,8 @@ import javax.persistence.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PersonBeforeVaccination extends Person {
 
+    @Builder
+    private PersonBeforeVaccination(String name, City city, Gender gender, int age, int phoneNumber) {
+        super(name, city, gender, age, phoneNumber);
+    }
 }
