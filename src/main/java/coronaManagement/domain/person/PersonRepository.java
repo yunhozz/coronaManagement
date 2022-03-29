f333122922ec412e95404420ebf4e6771b8aa90a
yunhozz <qkrdbsgh1121@naver.com>
2022-03-29 20:20:42 +0900

    first commit

--- src/main/java/coronaManagement/domain/person/PersonRepository.java
+++ src/main/java/coronaManagement/domain/person/PersonRepository.java
@@ -1,8 +1,13 @@
 package coronaManagement.domain.person;
 
-import coronaManagement.domain.person.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
+    @Query("select p from Person p where p.vaccinationCount < :vaccinationCount and p.vaccination > 0")
+    List<Person> findPersonWhoVaccinationTarget(@Param("vaccinationCount") int vaccinationCount);
 }
