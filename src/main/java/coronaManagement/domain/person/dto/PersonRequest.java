9ca1309c31f16ed6966de312ba5c4b6f0af6207f
yunhozz <qkrdbsgh1121@naver.com>
2022-04-20 17:50:04 +0900

    first commit

--- src/main/java/coronaManagement/domain/person/dto/PersonRequest.java
+++ src/main/java/coronaManagement/domain/person/dto/PersonRequest.java
@@ -1,15 +1,10 @@
 package coronaManagement.domain.person.dto;
 
-import coronaManagement.domain.record.EachRecord;
-import coronaManagement.domain.routeInformation.Address;
-import coronaManagement.domain.routeInformation.RouteInformation;
-import coronaManagement.domain.vaccine.Vaccine;
-import coronaManagement.domain.virus.Virus;
+import coronaManagement.domain.person.*;
 import coronaManagement.global.enums.City;
 import coronaManagement.global.enums.Gender;
 import coronaManagement.global.enums.InfectionStatus;
 import coronaManagement.global.enums.PhysicalStatus;
-import coronaManagement.domain.person.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -20,25 +15,21 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class PersonRequest {
 
+    private Long personId;
     @NotEmpty private String name;
     @NotEmpty private City city;
     @NotEmpty private Gender gender;
     @NotEmpty private int age;
     @NotEmpty private String phoneNumber;
 
-    private EachRecord eachRecord;
-
     //vaccinationPerson
-    private Vaccine vaccine;
     private int vaccinationCount;
     private LocalDateTime vaccinationDate;
 
     //infectedPerson
-    private Virus virus;
     private String distinguishId;
 
     //contactedPerson
-    private RouteInformation routeInformation;
     private int year;
     private int month;
     private int day;
@@ -52,8 +43,6 @@ public class PersonRequest {
                 .gender(gender)
                 .age(age)
                 .phoneNumber(phoneNumber)
-                .vaccine(vaccine)
-                .eachRecord(eachRecord)
                 .vaccinationCount(1)
                 .vaccinationDate(LocalDateTime.now())
                 .infectionStatus(InfectionStatus.BEFORE_INFECT)
@@ -78,8 +67,6 @@ public class PersonRequest {
                 .gender(gender)
                 .age(age)
                 .phoneNumber(phoneNumber)
-                .virus(virus)
-                .eachRecord(eachRecord)
                 .infectedTime(LocalDateTime.now())
                 .physicalStatus(PhysicalStatus.INFECTED)
                 .distinguishId(distinguishId)
@@ -93,7 +80,6 @@ public class PersonRequest {
                 .gender(gender)
                 .age(age)
                 .phoneNumber(phoneNumber)
-                .routeInformation(routeInformation)
                 .contactDate(LocalDateTime.of(year, month, day, hour, minute))
                 .infectionStatus(InfectionStatus.BEFORE_INFECT)
                 .build();
